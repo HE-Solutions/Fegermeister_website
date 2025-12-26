64a2d8b2afcbcd764fd6a55998b824890519ae82
Peter <hi@p3h3.de>
2025-12-26 21:29:47 +0100

    added "output: export"

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    output: "export",
+    trailingSlash: true,       // .../seite/index.html
+    images: { unoptimized: true }, // nötig für Static Export
 }
 
 export default nextConfig
